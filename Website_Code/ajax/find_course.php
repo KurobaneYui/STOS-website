1f909ea22c116cfcd55d28a36a1b5391659be726
KurobaneYui <1722716737@qq.com>
2019-11-04 00:48:35 +0800

    修正
    1、修正了python数据导入导出功能，查课排班导入可以去除空字符串
    2、查课数据展示修改为起始日期和结束日期范围内的所有数据
    3、查课数据展示的早自习数据可以去除非数字字符，出勤率计算鲁棒性更高

--- Website_Code/ajax/find_course.php
+++ Website_Code/ajax/find_course.php
@@ -3,15 +3,16 @@
 require("../frame/SQL_connect_frame.php");
 require("../frame/tools.php");
 
-function temp_getdata_xueyuan($xueyuan,$nianji,$time) { // 例如：$xueyuan="信通", $nianji='2018', $time="2019-04-03"
+function temp_getdata_xueyuan($xueyuan,$nianji,$time_start,$time_end) { // 例如：$xueyuan="信通", $nianji='2018', $time_start=$time_end="2019-04-03"
     $xinxiku = new STOS_MySQL();
     $shujuku = new STOS_MySQL_data();
 
     //搜索学院对应的教室
-    if($result = $shujuku->search("查课排班",false,array("学院"=>$xueyuan,"年级"=>$nianji,"日期"=>$time),false)) {
+    if($result = $shujuku->execute_query("SELECT * FROM `查课排班` WHERE 学院='{$xueyuan}' and 年级='{$nianji}' and 日期 BETWEEN '{$time_start}' and '{$time_end}';")) {
+//    if($result = $shujuku->search("查课排班",false,array("学院"=>$xueyuan,"年级"=>$nianji,"日期"=>$time),false)) {
         $returns = array();
         while($jiaoshi_d = $result->fetch_assoc()) {
-            if($t = $shujuku->search("查课数据",array("时段与上课周","教学楼","区号","教室编号","教室数据"),array("教学楼"=>$jiaoshi_d["教学楼"],"区号"=>$jiaoshi_d["区号"],"教室编号"=>$jiaoshi_d["教室编号"],"日期"=>$time),false)) {
+            if($t = $shujuku->search("查课数据",array("日期","时段与上课周","教学楼","区号","教室编号","教室数据"),array("教学楼"=>$jiaoshi_d["教学楼"],"区号"=>$jiaoshi_d["区号"],"教室编号"=>$jiaoshi_d["教室编号"],"日期"=>$jiaoshi_d["日期"]),false)) {
                 if($t = $t->fetch_assoc()) {
                     $data = json_decode($t["教室数据"],true);
                     unset($t["教室数据"]);
@@ -31,7 +32,7 @@ $nj = $_GET ['nj'];
 $date = $_GET['date'];
 $date_start = $_GET['date_start'];
 $date_end = $_GET['date_end'];
-echo temp_getdata_xueyuan($xy,$nj,$date);
+echo temp_getdata_xueyuan($xy,$nj,$date_start,$date_end);
 //response.write(temp_getdata_xueyuan($xy,$date));
 ?>
 
--- Website_Code/program/python3/test.py
+++ Website_Code/program/python3/test.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import TencentCloud_php, datetime, os, time
+import TencentCloud_php
+import os
+import time
 
-zaozixi = TencentCloud_php.zaozixi()
-chake = TencentCloud_php.chake()
+zaozixi = TencentCloud_php.ZaoZiXi()
+chake = TencentCloud_php.ChaKe()
 
-while(1):
+while 1:
     print("*********************")
     print("选择功能：")
     print("1)早自习数据导出")
@@ -22,59 +24,59 @@ while(1):
     print("q/Q)退出程序")
     print("*********************")
     a = input()
-    if a=='1' :
+    if a == '1':
         print()
         print("选择了早自习数据导出，请输入导出的文件夹目录：")
         path = input()
         if os.path.isdir(path):
-            zaozixi.download(path) # 下载人数和缺勤数据到指定文件夹下
+            zaozixi.download(path)  # 下载人数和缺勤数据到指定文件夹下
         else:
             print("文件夹不存在")
-    elif a=='2' :
+    elif a == '2':
         print()
         print("选择了查课数据导出，请输入导出的文件夹目录：")
         path = input()
         if os.path.isdir(path):
-            chake.download(path) # 下载教室数据到指定文件夹下
+            chake.download(path)  # 下载教室数据到指定文件夹下
         else:
             print("文件夹不存在")
-    elif a=='3' :
+    elif a == '3':
         print()
         print("选择了各组空课表导出，请输入导出的文件夹目录：")
         path = input()
         if os.path.isdir(path):
-            chake.download_kongkebiao(path) # 下载教室数据到指定文件夹下
+            chake.download_kongkebiao(path)  # 下载教室数据到指定文件夹下
         else:
             print("文件夹不存在")
-    elif a=='4' :
+    elif a == '4':
         print()
         print("选择了早自习教室信息导入，请输入导入的文件目录：")
         path = input()
         if os.path.isfile(path):
-            zaozixi.work_info(path) # 导入早自习教室安排
+            zaozixi.work_info(path)  # 导入早自习教室安排
         else:
             print("文件夹不存在")
-    elif a=='5' :
+    elif a == '5':
         print()
         print("选择了早自习排班导入")
-        zaozixi.work_schedule_auto() # 导入查课教室安排
-    elif a=='6' :
+        zaozixi.work_schedule_auto()  # 导入查课教室安排
+    elif a == '6':
         print()
         print("选择了查课课程信息导入，请输入导入的文件目录：")
         path = input()
         if os.path.isfile(path):
-            chake.work_info(path) # 自动生成排班数据，并导入
+            chake.work_info(path)  # 自动生成排班数据，并导入
         else:
             print("文件夹不存在")
-    elif a=='7' :
+    elif a == '7':
         print()
         print("选择了查课排班导入，请输入导入的文件目录：")
         path = input()
         if os.path.isfile(path):
-            chake.work_schedule(path) # 根据excel格式的排班信息进行排班
+            chake.work_schedule(path)  # 根据excel格式的排班信息进行排班
         else:
             print("文件夹不存在")
-    elif a=='q' or a=='Q' :
+    elif a == 'q' or a == 'Q':
         print()
         print("选择了退出程序，2秒后退出")
         time.sleep(2)
