1b6aae71125f7e8df32963c8d522749fc41b96a8
KurobaneYui <1722716737@qq.com>
2021-09-21 18:01:05 +0800

    update authorityManagement.html > reference authorityManagement.js
    finish authorityManagement.js
    > complete all functions of this file
    
    finish membersManagement.js
    > complete all functions of this file
    
    fix membersManagement.html
    > change an expression
    > add ID for a div tag
    
    update changeMemberOrAuth.php
    > better format

--- Ajax/Users/changeMemberOrAuth.php
+++ Ajax/Users/changeMemberOrAuth.php
@@ -10,59 +10,57 @@ require_once ROOT_PATH . "/Frame/php/CustomPackAndLogger/STSA_log.php";
 $logger = new STSA_log();
 
 if (isset($_POST['requestFunction'])) { // 是否有要请求的类别
-    if ($_POST['requestFunction']==='getFullMembersAuth') {
+    if ($_POST['requestFunction'] === 'getFullMembersAuth') {
         require_once ROOT_PATH . '/Frame/php/Users/changeAuth.php';
         try {
             $returns = new UnionReturnInterface();
             $returns->setData(showMemberAuth());
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-changeMemberOrAuth, function execute successfully", "Log");
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-changeMemberOrAuth, function execute successfully", "Log");
             echo $returns;
         } catch (JsonException $e) {
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-changeMemberOrAuth, json相关错误, 错误信息:\n{$e}", "Error");
-            $returns = new UnionReturnInterface('417','数据封装过程中出现错误');
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-changeMemberOrAuth, json相关错误, 错误信息:\n{$e}", "Error");
+            $returns = new UnionReturnInterface('417', '数据封装过程中出现错误');
             echo $returns;
         } catch (STSAException $e) {
             $returns = new UnionReturnInterface();
             $returns->boundSTSAException($e);
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-changeMemberOrAuth, meet customException, 错误信息:\n{$returns}", "Warning");
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-changeMemberOrAuth, meet customException, 错误信息:\n{$returns}", "Warning");
             echo $returns;
         }
-    }
-    elseif ($_POST['requestFunction']==='changeAuth') {
+    } elseif ($_POST['requestFunction'] === 'changeAuth') {
         require_once ROOT_PATH . '/Frame/php/Users/getGroupCode.php';
         require_once ROOT_PATH . '/Frame/php/Users/changeAuth.php';
         try {
             $returns = new UnionReturnInterface();
             // 从POST信息里获取数据
-            if (!isset($_POST["personID"],$_POST["groupName"],$_POST["work"])) {
-                $logger->add_log(__FILE__.":".__LINE__, "Ajax-changeAuth, 没有提供需要的数据", "Log");
+            if (!isset($_POST["personID"], $_POST["groupName"], $_POST["work"])) {
+                $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-changeAuth, 没有提供需要的数据", "Log");
                 $returns = new UnionReturnInterface('400', "没有提供需要的数据");
                 echo $returns;
                 exit;
             }
-            if ($_POST["work"]!=="team" && $_POST["work"]!=="group" && $_POST["work"]!=="member") {
-                $logger->add_log(__FILE__.":".__LINE__, "Ajax-changeAuth, 没有提供正确的数据", "Log");
+            if ($_POST["work"] !== "team" && $_POST["work"] !== "group" && $_POST["work"] !== "member") {
+                $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-changeAuth, 没有提供正确的数据", "Log");
                 $returns = new UnionReturnInterface('400', "没有提供正确的数据");
                 echo $returns;
                 exit;
             }
-            $authPack = ["+",$_POST["work"]];
+            $authPack = ["+", $_POST["work"]];
             // 执行主体
-            $returns->setData(changeMemberAuth($_POST["personID"],$authPack,getGroupCode($_POST["groupName"])));
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-changeAuth, function execute successfully", "Log");
+            $returns->setData(changeMemberAuth($_POST["personID"], $authPack, getGroupCode($_POST["groupName"])));
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-changeAuth, function execute successfully", "Log");
             echo $returns;
         } catch (JsonException $e) {
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-changeAuth, json相关错误, 错误信息:\n{$e}", "Error");
-            $returns = new UnionReturnInterface('417','数据封装过程中出现错误');
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-changeAuth, json相关错误, 错误信息:\n{$e}", "Error");
+            $returns = new UnionReturnInterface('417', '数据封装过程中出现错误');
             echo $returns;
         } catch (STSAException $e) {
             $returns = new UnionReturnInterface();
             $returns->boundSTSAException($e);
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-changeAuth, meet customException, 错误信息:\n{$returns}", "Warning");
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-changeAuth, meet customException, 错误信息:\n{$returns}", "Warning");
             echo $returns;
         }
-    }
-    elseif ($_POST['requestFunction']==='getGroupFullMembersForWorkChange') {
+    } elseif ($_POST['requestFunction'] === 'getGroupFullMembersForWorkChange') {
         require_once ROOT_PATH . '/Frame/php/Users/getGroupCode.php';
         require_once ROOT_PATH . '/Frame/php/Users/getPersonInfo.php';
         try {
@@ -72,76 +70,74 @@ if (isset($_POST['requestFunction'])) { // 是否有要请求的类别
             foreach ($groupCodeResult as $value) {
                 $tmp = getGroupPersonBasicInfo($value);
                 $rows = $tmp["行数"];
-                $fields = ["姓名","学号","性别","部门名称"];
+                $fields = ["姓名", "学号", "性别", "部门名称"];
                 $cols = count($fields);
-                $name = array_column($tmp["数据"],"姓名");
-                $ID = array_column($tmp["数据"],"学号");
-                $gender = array_column($tmp["数据"],"性别");
-                $groupName = array_column($tmp["数据"],"部门名称");
+                $name = array_column($tmp["数据"], "姓名");
+                $ID = array_column($tmp["数据"], "学号");
+                $gender = array_column($tmp["数据"], "性别");
+                $groupName = array_column($tmp["数据"], "部门名称");
                 $tmp = [];
-                foreach ($name as $key=>$value_) {
+                foreach ($name as $key => $value_) {
                     $tmp[$key]["姓名"] = $value_;
                     $tmp[$key]["学号"] = $ID[$key];
                     $tmp[$key]["性别"] = $gender[$key];
                     $tmp[$key]["部门名称"] = $groupName[$key];
                 }
-                $InfosInGroups[] = ["行数"=>$rows,"列数"=>$cols,"字段"=>$fields,"数据"=>$tmp];
+                $InfosInGroups[] = ["行数" => $rows, "列数" => $cols, "字段" => $fields, "数据" => $tmp];
             }
             $returns->setData($InfosInGroups);
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-getGroupFullMembersForWorkChange, function execute successfully", "Log");
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-getGroupFullMembersForWorkChange, function execute successfully", "Log");
             echo $returns;
         } catch (JsonException $e) {
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-getGroupFullMembersForWorkChange, json相关错误, 错误信息:\n{$e}", "Error");
-            $returns = new UnionReturnInterface('417','数据封装过程中出现错误');
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-getGroupFullMembersForWorkChange, json相关错误, 错误信息:\n{$e}", "Error");
+            $returns = new UnionReturnInterface('417', '数据封装过程中出现错误');
             echo $returns;
         } catch (STSAException $e) {
             $returns = new UnionReturnInterface();
             $returns->boundSTSAException($e);
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-getGroupFullMembersForWorkChange, meet customException, 错误信息:\n{$returns}", "Warning");
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-getGroupFullMembersForWorkChange, meet customException, 错误信息:\n{$returns}", "Warning");
             echo $returns;
         }
-    }
-    elseif ($_POST['requestFunction']==='getPreMembersForWorkChange') {
+    } elseif ($_POST['requestFunction'] === 'getPreMembersForWorkChange') {
         require_once ROOT_PATH . '/Frame/php/Users/getPersonInfo.php';
         try {
             $returns = new UnionReturnInterface();
             $tmp = getPrePersonBasicInfo();
             $rows = $tmp["行数"];
-            $fields = ["姓名","学号","性别"];
+            $fields = ["姓名", "学号", "性别"];
             $cols = count($fields);
-            $name = array_column($tmp["数据"],"姓名");
-            $ID = array_column($tmp["数据"],"学号");
-            $gender = array_column($tmp["数据"],"性别");
+            $name = array_column($tmp["数据"], "姓名");
+            $ID = array_column($tmp["数据"], "学号");
+            $gender = array_column($tmp["数据"], "性别");
             $tmp = [];
-            foreach ($name as $key=>$value_) {
+            foreach ($name as $key => $value_) {
                 $tmp[$key]["姓名"] = $value_;
                 $tmp[$key]["学号"] = $ID[$key];
                 $tmp[$key]["性别"] = $gender[$key];
             }
-            $returns->setData(["行数"=>$rows,"列数"=>$cols,"字段"=>$fields,"数据"=>$tmp]);
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-getPreMembersForWorkChange, function execute successfully", "Log");
+            $returns->setData(["行数" => $rows, "列数" => $cols, "字段" => $fields, "数据" => $tmp]);
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-getPreMembersForWorkChange, function execute successfully", "Log");
             echo $returns;
         } catch (JsonException $e) {
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-getPreMembersForWorkChange, json相关错误, 错误信息:\n{$e}", "Error");
-            $returns = new UnionReturnInterface('417','数据封装过程中出现错误');
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-getPreMembersForWorkChange, json相关错误, 错误信息:\n{$e}", "Error");
+            $returns = new UnionReturnInterface('417', '数据封装过程中出现错误');
             echo $returns;
         } catch (STSAException $e) {
             $returns = new UnionReturnInterface();
             $returns->boundSTSAException($e);
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-getPreMembersForWorkChange, meet customException, 错误信息:\n{$returns}", "Warning");
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-getPreMembersForWorkChange, meet customException, 错误信息:\n{$returns}", "Warning");
             echo $returns;
         }
-    }
-    elseif ($_POST['requestFunction']==='searchMember') {
+    } elseif ($_POST['requestFunction'] === 'searchMember') {
         require_once ROOT_PATH . '/Frame/php/Users/changeGroupMember.php';
         if (!isset($_POST["searchString"])) {
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-searchMember, 没有提供需要的数据", "Log");
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-searchMember, 没有提供需要的数据", "Log");
             $returns = new UnionReturnInterface('400', "没有提供需要的数据");
             echo $returns;
             exit;
         }
-        if ($_POST["searchString"]==="") {
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-searchMember, 没有提供正确的数据", "Log");
+        if ($_POST["searchString"] === "") {
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-searchMember, 没有提供正确的数据", "Log");
             $returns = new UnionReturnInterface('400', "没有提供正确的数据");
             echo $returns;
             exit;
@@ -149,24 +145,23 @@ if (isset($_POST['requestFunction'])) { // 是否有要请求的类别
         try {
             $returns = new UnionReturnInterface();
             $returns->setData(searchPreMember($_POST["searchString"]));
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-searchMember, function execute successfully", "Log");
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-searchMember, function execute successfully", "Log");
             echo $returns;
         } catch (JsonException $e) {
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-searchMember, json相关错误, 错误信息:\n{$e}", "Error");
-            $returns = new UnionReturnInterface('417','数据封装过程中出现错误');
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-searchMember, json相关错误, 错误信息:\n{$e}", "Error");
+            $returns = new UnionReturnInterface('417', '数据封装过程中出现错误');
             echo $returns;
         } catch (STSAException $e) {
             $returns = new UnionReturnInterface();
             $returns->boundSTSAException($e);
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-searchMember, meet customException, 错误信息:\n{$returns}", "Warning");
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-searchMember, meet customException, 错误信息:\n{$returns}", "Warning");
             echo $returns;
         }
-    }
-    elseif ($_POST['requestFunction']==='addMember') {
+    } elseif ($_POST['requestFunction'] === 'addMember') {
         require_once ROOT_PATH . '/Frame/php/Users/getGroupCode.php';
         require_once ROOT_PATH . '/Frame/php/Users/changeGroupMember.php';
-        if (!isset($_POST["personID"],$_POST["groupName"])) {
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-addMember, 没有提供需要的数据", "Log");
+        if (!isset($_POST["personID"], $_POST["groupName"])) {
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-addMember, 没有提供需要的数据", "Log");
             $returns = new UnionReturnInterface('400', "没有提供需要的数据");
             echo $returns;
             exit;
@@ -175,20 +170,19 @@ if (isset($_POST['requestFunction'])) { // 是否有要请求的类别
             $returns = new UnionReturnInterface();
             $groupCodeResult = getGroupCode($_POST["groupName"]);
             $returns->setData(addGroupMember($_POST["personID"], $groupCodeResult));
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-addMember, function execute successfully", "Log");
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-addMember, function execute successfully", "Log");
             echo $returns;
         } catch (JsonException $e) {
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-addMember, json相关错误, 错误信息:\n{$e}", "Error");
-            $returns = new UnionReturnInterface('417','数据封装过程中出现错误');
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-addMember, json相关错误, 错误信息:\n{$e}", "Error");
+            $returns = new UnionReturnInterface('417', '数据封装过程中出现错误');
             echo $returns;
         } catch (STSAException $e) {
             $returns = new UnionReturnInterface();
             $returns->boundSTSAException($e);
-            $logger->add_log(__FILE__.":".__LINE__, "Ajax-addMember, meet customException, 错误信息:\n{$returns}", "Warning");
+            $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-addMember, meet customException, 错误信息:\n{$returns}", "Warning");
             echo $returns;
         }
-    }
-    elseif ($_POST['requestFunction']==='removeMember') {
+    } elseif ($_POST['requestFunction'] === 'removeMember') {
         require_once ROOT_PATH . '/Frame/php/Users/getGroupCode.php';
         require_once ROOT_PATH . '/Frame/php/Users/changeGroupMember.php';
         if (!isset($_POST["personID"], $_POST["groupName"])) {
@@ -213,15 +207,13 @@ if (isset($_POST['requestFunction'])) { // 是否有要请求的类别
             $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-removeMember, meet customException, 错误信息:\n{$returns}", "Warning");
             echo $returns;
         }
-    }
-    else {
-        $logger->add_log(__FILE__.":".__LINE__, "Ajax-changeMemberOrAuth, 功能不存在", "Log");
+    } else {
+        $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-changeMemberOrAuth, 功能不存在", "Log");
         $returns = new UnionReturnInterface('404', "功能不存在");
         echo $returns;
     }
-}
-else {
-    $logger->add_log(__FILE__.":".__LINE__, "Ajax-changeMemberOrAuth, 没有选择需要的功能", "Log");
+} else {
+    $logger->add_log(__FILE__ . ":" . __LINE__, "Ajax-changeMemberOrAuth, 没有选择需要的功能", "Log");
     $returns = new UnionReturnInterface('404', "没有选择需要的功能");
     echo $returns;
 }
