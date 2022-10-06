9ea8af17a2da9c288785cdbb6a8d04a6a6ee035c
Kurobane Yui <1722716737@qq.com>
2022-10-07 01:31:43 +0800

    [update] update run.py: move app secret key to config file
    [fix]
    fix duplicate function name

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 import flask
 from flask import Flask, request, redirect, send_file, url_for, abort, session, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
 from Ajax.Users import Users
 from Frame.python3.Authorization import checkIfLogin
 from datetime import timedelta
+import json
 
 
 # set template_folder for 'render_template' function
 app = Flask(__name__, template_folder="Frame/html5/")
-# set secret_key for starting the session
-app.secret_key = b'_5#"F4Q8z\n\xec]/'
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = timedelta(seconds=0.5)
 
 
@@ -37,7 +37,7 @@ def AssetsRoutes(additionalURL):
     return send_file("assets/"+additionalURL)
 
 
-@app.route("/Users/<path:additionalURL>", methods=['GET','POST'])
+@app.route("/Users/<path:additionalURL>", methods=['GET', 'POST'])
 def UsersRoutes(additionalURL):
     if additionalURL == "Authentication/login.html" and checkIfLogin()["data"]:
         return redirect("/Users/UserCenter/index.html")
@@ -52,19 +52,29 @@ def index():
 
 # handle when '404 not found' error
 @app.errorhandler(404)
-def not_found(error):
+def not_found_404(error):
     print("Try to access not exist URL: ", flask.request.url)
     return render_template("404.html"), 404
 
 # handle when '405 not found' error
+
+
 @app.errorhandler(405)
-def not_found(error):
+def not_found_405(error):
     print("Try to access URL with illegal method: ", flask.request.url)
     return render_template("405.html"), 405
 
 
 if __name__ == "__main__":
+    with open("./config/Flask.conf", 'r') as f:
+        config = json.load(f)
+    if config["proxy_enable"]:
+        app.wsgi_app = ProxyFix(
+            app.wsgi_app, x_for=config["x_for"], x_proto=config["x_proto"], x_host=config["x_host"], x_prefix=config["x_prefix"])
+    # set secret_key for starting the session
+    app.secret_key = config["secret_key"].encode()
     # Users package include ajax handler for user function
     Users(app)
     # start a request
-    app.run(debug=True, threaded=True, host="127.0.0.1", port=5000)
+    app.run(debug=config["debug"], threaded=config["threaded"],
+            host=config["host"], port=config["port"])
