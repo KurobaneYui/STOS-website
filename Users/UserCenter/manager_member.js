2b86cf7df303b7185b2d63e53056ed609a9edf57
Kurobane Yui <1722716737@qq.com>
2022-10-18 00:16:04 +0800

    update manager_member page template

---
+++ Users/UserCenter/manager_member.js
@@ -0,0 +1,33 @@
+function get_all_groups_members() {
+    ;
+}
+
+
+function add_table_template() {
+    ;
+}
+
+
+function fill_table_by_group_id() {
+    ;
+}
+
+
+function search_member() {
+    ;
+}
+
+
+function fill_search_results() {
+    ;
+}
+
+
+function add_member() {
+    ;
+}
+
+
+function remove_member() {
+    ;
+}
