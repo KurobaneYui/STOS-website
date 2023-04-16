a28562322c53cb5170d8a8b768a293342674ce8d
Kurobane Yui <1722716737@qq.com>
2023-04-17 07:06:05 +0800

    [fix] 去除无用代码

--- Users/UserCenter/courses_record.js
+++ Users/UserCenter/courses_record.js
@@ -1,14 +1,5 @@
 $(function () {
     get_records();
-    Date.prototype.format = function (formatStr) {
-        var str = formatStr;
-        var Week = ['日', '一', '二', '三', '四', '五', '六'];
-        str = str.replace(/yyyy|YYYY/, this.getFullYear());
-        str = str.replace(/MM/, (this.getMonth() + 1) > 9 ? (this.getMonth() + 1).toString() : '0' + (this.getMonth() + 1));
-        str = str.replace(/dd|DD/, this.getDate() > 9 ? this.getDate().toString() : '0' + this.getDate());
-        return str;
-    }
-    let currentDate = (new Date()).format("yyyy-MM-dd");
 })
 
 function get_records() {
