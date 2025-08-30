f340071fb3ed795e0f74e621c14759a7d56233a3
Kurobane Yui <1722716737@qq.com>
2025-08-30 20:51:43 +0800

    开始构建个人信息页

--- src/Register.vue
+++ src/Register.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { ref, reactive, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
 // ------ 表单数据 ------
-const form = reactive({
+const form = ref({
     name: '',
     studentID: '',
     gender: 'male',
@@ -89,7 +89,7 @@ onMounted(() => {
 // ---- 注册函数 ----
 async function register() {
     claimError.value = false
-    if (!form.claimBox) {
+    if (!form.value.claimBox) {
         claimError.value = true
         if (window.sweetAlert) sweetAlert("请阅读个人信息说明");
         else if (window.swal) swal({ title: "请阅读个人信息说明", icon: "info" });
@@ -97,21 +97,21 @@ async function register() {
     }
 
     const register_info = {
-        name: form.name,
-        studentID: form.studentID,
-        gender: form.gender === 'male' ? '男' : '女',
-        ethnicity: form.ethnicity,
-        hometown: form.hometown,
-        phone: form.phone,
-        qq: form.qq,
-        campus: form.campus,
-        school: form.school,
-        dormitory_yuan: form.dormitory_yuan,
-        dormitory_dong: String(form.dormitory_dong),
-        dormitory_hao: String(form.dormitory_hao),
-        bank: form.bank,
-        subsidyDossier: form.subsidyDossier,
-        password: form.password
+        name: form.value.name,
+        studentID: form.value.studentID,
+        gender: form.value.gender === 'male' ? '男' : '女',
+        ethnicity: form.value.ethnicity,
+        hometown: form.value.hometown,
+        phone: form.value.phone,
+        qq: form.value.qq,
+        campus: form.value.campus,
+        school: form.value.school,
+        dormitory_yuan: form.value.dormitory_yuan,
+        dormitory_dong: String(form.value.dormitory_dong),
+        dormitory_hao: String(form.value.dormitory_hao),
+        bank: form.value.bank,
+        subsidyDossier: form.value.subsidyDossier,
+        password: form.value.password
     }
 
     try {
@@ -176,12 +176,12 @@ function onSubmit(e) {
                         <div class="mb-3">
                             <label for="username" class="form-label">姓名</label>
                             <input type="text" class="form-control" id="username" name="username" placeholder="请输入姓名"
-                                autofocus required v-model="form.name" />
+                                autofocus required v-model="form.value.name" />
                         </div>
                         <div class="mb-3">
                             <label for="id" class="form-label">学号</label>
                             <input type="text" class="form-control" id="id" name="id" placeholder="请输入学号" required
-                                v-model="form.studentID" />
+                                v-model="form.value.studentID" />
                         </div>
                         <div class="mb-3">
                             <label class="form-label">性别</label>
@@ -189,25 +189,25 @@ function onSubmit(e) {
                                 <div class="form-check col-2 offset-1">
                                     <label for="gender_male" class="form-check-label">男</label>
                                     <input type="radio" class="form-check-input" id="gender_male" name="gender"
-                                        value="male" v-model="form.gender" />
+                                        value="male" v-model="form.value.gender" />
                                 </div>
                                 <div class="form-check col-2">
                                     <label for="gender_female" class="form-check-label">女</label>
                                     <input type="radio" class="form-check-input" id="gender_female" name="gender"
-                                        value="female" v-model="form.gender" />
+                                        value="female" v-model="form.value.gender" />
                                 </div>
                             </div>
                         </div>
                         <div class="mb-3">
                             <label for="ethnicity" class="form-label">民族</label>
                             <input type="text" class="form-control" id="ethnicity" name="ethnicity" placeholder="请输入民族"
-                                aria-describedby="ethnicityHelp" required v-model="form.ethnicity" />
+                                aria-describedby="ethnicityHelp" required v-model="form.value.ethnicity" />
                             <div id="ethnicityHelp" class="form-text">完整填写，如：填写“汉族”，而不是“汉”</div>
                         </div>
                         <div class="mb-3">
                             <label for="hometown" class="form-label">籍贯</label>
                             <input type="text" class="form-control" id="hometown" name="hometown" placeholder="请输入籍贯"
-                                required v-model="form.hometown" />
+                                required v-model="form.value.hometown" />
                             <div id="ethnicityHelp" class="form-text">精确至：省市（县）</div>
                         </div>
                     </div>
@@ -221,32 +221,32 @@ function onSubmit(e) {
                         <div class="mb-3">
                             <label for="phone" class="form-label">电话</label>
                             <input type="tel" class="form-control" id="phone" name="phone" placeholder="请输入电话" required
-                                v-model="form.phone" />
+                                v-model="form.value.phone" />
                         </div>
                         <div class="mb-3">
                             <label for="qq" class="form-label">QQ</label>
                             <input type="text" class="form-control" id="qq" name="qq" placeholder="请输入QQ号" required
-                                v-model="form.qq" />
+                                v-model="form.value.qq" />
                         </div>
                         <div class="mb-3">
                             <label for="campus" class="form-label">校区</label>
-                            <select id="campus" class="form-select" required v-model="form.campus">
+                            <select id="campus" class="form-select" required v-model="form.value.campus">
                                 <option v-for="item in campusOptions" :key="item.value" :value="item.value">{{ item.text
-                                }}</option>
+                                    }}</option>
                             </select>
                         </div>
                         <div class="mb-3">
                             <label for="school" class="form-label">学院</label>
-                            <select id="school" class="form-select" required v-model="form.school">
+                            <select id="school" class="form-select" required v-model="form.value.school">
                                 <option v-for="item in schoolOptions" :key="item.value" :value="item.value">{{ item.text
-                                }}</option>
+                                    }}</option>
                             </select>
                         </div>
                         <div class="mb-3">
                             <label for="dormitory" class="form-label">寝室</label>
                             <div id="dormitory" class="input-group">
                                 <select id="dormitory_yuan" class="form-control" aria-describedby="dormitoryHelp"
-                                    required v-model="form.dormitory_yuan">
+                                    required v-model="form.value.dormitory_yuan">
                                     <option value="学知苑">学知苑</option>
                                     <option value="硕丰苑">硕丰苑</option>
                                     <option value="校内">沙河校内</option>
@@ -254,10 +254,10 @@ function onSubmit(e) {
                                 </select>
                                 <span class="input-group-text">苑</span>
                                 <input id="dormitory_dong" type="number" class="form-control" min="1" max="99"
-                                    placeholder="楼栋" aria-label="dong" required v-model="form.dormitory_dong" />
+                                    placeholder="楼栋" aria-label="dong" required v-model="form.value.dormitory_dong" />
                                 <span class="input-group-text">栋</span>
                                 <input id="dormitory_hao" type="number" class="form-control" min="1" max="999"
-                                    placeholder="宿舍号" aria-label="hao" required v-model="form.dormitory_hao" />
+                                    placeholder="宿舍号" aria-label="hao" required v-model="form.value.dormitory_hao" />
                                 <span class="input-group-text">号</span>
                             </div>
                             <div id="dormitoryHelp" class="form-text">第一空若为“校外”，则后两空填0</div>
@@ -275,7 +275,7 @@ function onSubmit(e) {
                                 <div class="mb-3">
                                     <label for="bank" class="form-label">银行卡号</label>
                                     <input type="text" class="form-control" id="bank" name="bank" placeholder="请输入银行卡号"
-                                        aria-describedby="bankHelp" required v-model="form.bank" />
+                                        aria-describedby="bankHelp" required v-model="form.value.bank" />
                                     <div id="bankHelp" class="form-text">建议学校的建行卡</div>
                                 </div>
                                 <div class="mb-3">
@@ -283,7 +283,7 @@ function onSubmit(e) {
                                         <label for="subsidyDossier" class="form-check-label">建档立卡</label>
                                         <input type="checkbox" class="form-check-input" id="subsidyDossier"
                                             name="subsidyDossier" aria-describedby="subsidyDossierHelp" required
-                                            v-model="form.subsidyDossier" />
+                                            v-model="form.value.subsidyDossier" />
                                         <div id="subsidyDossierHelp" class="form-text">
                                             扶贫政策，如有办理建档立卡则选择。如没听说过，大概率不是，不用选择。不确定请联系辅导员询问。
                                             <span class="fw-bold text-primary">数据会核实，请勿作假</span>
@@ -302,7 +302,7 @@ function onSubmit(e) {
                                     <div class="input-group input-group-merge">
                                         <input type="password" id="password" class="form-control" name="password"
                                             placeholder="请设置登录密码" aria-describedby="passwordHelp"
-                                            v-model="form.password" />
+                                            v-model="form.value.password" />
                                         <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
                                     </div>
                                     <div id="passwordHelp" class="form-text">
@@ -321,7 +321,7 @@ function onSubmit(e) {
                         <div class="mb-3 d-flex justify-content-center">
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms"
-                                    v-model="form.claimBox" />
+                                    v-model="form.value.claimBox" />
                                 <label class="form-check-label" :for="'terms-conditions'"
                                     :style="{ color: claimError ? 'red' : '' }">
                                     阅读
@@ -353,7 +353,7 @@ function onSubmit(e) {
                                         </div>
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-primary" data-bs-dismiss="modal"
-                                                @click="form.claimBox = true">已知晓
+                                                @click="form.value.claimBox = true">已知晓
                                             </button>
                                         </div>
                                     </div>
