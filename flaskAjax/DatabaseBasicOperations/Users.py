6dc9eaf1e805de66b60d70dd35cf715e19593c18
Kurobane Yui <1722716737@qq.com>
2025-09-03 18:24:36 +0800

    修正重置密码逻辑

--- flaskAjax/DatabaseBasicOperations/Users.py
+++ flaskAjax/DatabaseBasicOperations/Users.py
@@ -269,40 +269,28 @@ class UsersDatabase:
         with session_context as session:
             # =====================
             # 利用提供的信息匹配用户
-            # DBAffectRows = database.execute(
-            #     "SELECT `MemberBasic`.student_id FROM `MemberBasic` \
-            #     LEFT JOIN `MemberExtend` ON `MemberExtend`.student_id = `MemberBasic`.student_id \
-            #     LEFT JOIN `School` ON `School`.school_id = `MemberExtend`.school_id \
-            #     WHERE MemberBasic.student_id = %(StudentID)s and MemberBasic.name = %(Name)s \
-            #         and School.name = %(School)s and hometown = %(Hometown)s;",
-            #     flaskRequest.form
-            # )
             assert flaskRequest.json is not None
             student_id = flaskRequest.json["StudentID"]
             name = flaskRequest.json["Name"]
             school = flaskRequest.json["School"]
             hometown = flaskRequest.json["Hometown"]
-            stmt = (
-                select(
-                    SQL_User.student_id,
-                )
-                .join(
-                    SQL_User.profile,
-                )
-                .join(
-                    SQL_UserProfile.college,
-                )
-                .where(
-                    SQL_User.student_id == student_id,
-                    SQL_User.name == name,
-                    SQL_UserProfile.hometown == hometown,
-                    SQL_College.name == school,
-                )
-            )
-            results = session.scalars(stmt).all()
             # ===================
             # 匹配结果不唯一则报错
+            results = (
+                session.query(SQL_UserProfile).filter_by(student_id=student_id).all()
+            )
             if len(results) != 1:
+                raise PermissionDenyError(
+                    "Information is wrong.",
+                    filename=__file__,
+                    line=sys._getframe().f_lineno,
+                )
+            results = session.query(SQL_User).filter_by(student_id=student_id).one()
+            if (
+                results.name != name
+                or results.profile.college.name != school
+                or results.profile.hometown != hometown
+            ):
                 raise PermissionDenyError(
                     "Information is wrong.",
                     filename=__file__,
